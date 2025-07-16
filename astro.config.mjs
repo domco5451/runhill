ca3e9f3a14f1db2cbbbfb55c4416293fc2bed4ba
domco5451 <domco.ko@gmail.com>
2025-07-16 13:57:27 +0200

    add changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://runhill.netlify.app/"
+    site: "https://runhill.netlify.app"
 });
